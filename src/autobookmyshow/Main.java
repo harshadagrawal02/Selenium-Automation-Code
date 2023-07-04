c76e555fdb91af1f58b95ca98d17663a2cee5a12
harshadagrawal02 <138512235+harshadagrawal02@users.noreply.github.com>
2023-07-04 14:40:28 +0530

    Update Main.java

--- src/autobookmyshow/Main.java
+++ src/autobookmyshow/Main.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
  
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver_win32\\chromedriver.exe");
 		
